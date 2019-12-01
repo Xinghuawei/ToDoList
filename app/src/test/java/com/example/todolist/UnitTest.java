eeb6b550924a98c21b4b80a382c2bd1dbd058e46
Xue <wxh52794@gmail.com>
2019-11-30 18:18:30 -0800

    test

--- app/src/test/java/com/example/todolist/UnitTest.java
+++ app/src/test/java/com/example/todolist/UnitTest.java
@@ -12,18 +12,19 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class UnitTest extends LoginActivity{
-    LoginActivity reg;
-    Boolean r;
-    public DatabaseHelper db;
-
-    private Boolean checkExist() {
-        db = new DatabaseHelper(this);
-        Boolean r = db.checkPasswordExist();
-        return r;
-    }
-    @Test
-    public void writeUserAndReadInList() {
-        checkExist();
-    }
+public class UnitTest{
+//    LoginActivity reg;
+////    Boolean r;
+////    public DatabaseHelper db;
+////
+////    private Boolean checkExist() {
+////        db = new DatabaseHelper(context);
+////        Boolean r = db.checkPasswordExist();
+////        return r;
+////    }
+////    @Test
+////    public void writeUserAndReadInList() {
+////        checkExist();
+////    }
+    
 }
